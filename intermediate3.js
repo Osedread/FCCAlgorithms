7e3c4f2371b67b99f04f2d9b3732dca3832785b7
Jose Diaz-Gonzalez <osedread@gmail.com>
2017-08-18 01:19:12 +0100

    Added n-1 numbers to the romanArray, still failing on +1000 numbers

---
+++ intermediate3.js
@@ -0,0 +1,43 @@
+// ROMAN NUMERAL CONVERTER
+// Convert the given number into a roman numeral.
+// All roman numerals answers should be provided in upper-case.
+// jshint esversion: 6
+//
+function convertToRoman(num) {
+    
+    let romanArr = [];
+
+    let ref = [
+        ['M', 1000],
+        ['CM', 900], // before M
+        ['D', 500],
+        ['CD', 400], // before D
+        ['C', 100],
+        ['XC', 90], // before C
+        ['L', 50],
+        ['XL', 40], // before L
+        ['X', 10],
+        ['IX', 9], // before X
+        ['V', 5],
+        ['IV', 4], // before V
+        ['I', 1]
+    ]; 
+    
+    for (let i=0; i < ref.length; i++) {
+        let romanRow = ref[i];
+        let romanVal = romanRow[1]; // 100;
+        let romanLetter = romanRow[0]; // C;
+        if (num >= romanVal) {
+            num -= romanVal;
+            romanArr.push(romanLetter);
+            i=0;
+        }
+    }
+
+    let result = romanArr.join("");
+    console.log(result);
+    return result;
+}
+
+convertToRoman(2014); // "MMXIV"
+convertToRoman(3999); // "MMMCMXCIX"
